7953d8a82afdd8f282bd4f99a8e67589e4b6eced
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2022-02-14 18:40:46 +0900

    add Navigation.vue(NuxtLink)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,26 @@
-<template lang="pug">
-.container
-  div
-  <NuxtLogo />
-  h1 {{`${title}`}}
+<template>
+  <div class="container">
+    <NuxtLogo />
+    <div>
+      <h1 class="mb-2 font-bold">
+        {{ `${title}` }}
+      </h1>
+      <P class="py-1 text-blue-600">{{ `${desc}` }}</P>
+      <button class="bg-blue-500 text-white font-normal py-2 py-4 rounded">
+        Push Me!
+      </button>
+    </div>
+    <Navigation />
+  </div>
 </template>
 
 <script>
 import NuxtLogo from '../components/NuxtLogo.vue'
+import Navigation from '../components/Navigation.vue'
+
 export default {
   name: 'IndexPage',
-  components: { NuxtLogo },
+  components: { NuxtLogo, Navigation },
   data() {
     return {
       title: 'Hello, world!',
