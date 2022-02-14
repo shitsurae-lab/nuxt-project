8053daf6bf8770e95c134cbc56b5c226496afc5e
shitsurae-lab <20566363+shitsurae-lab@users.noreply.github.com>
2022-02-14 19:13:30 +0900

    記述を<Nuxt />内に記述することとしてリライト

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,36 @@
 <template>
-  <div>
-    <h1>
-      {{ `${title}` }}
-    </h1>
+  <div class="container">
+    <div>
+      <h1 class="mb-2 font-bold">
+        {{ `${title}` }}
+      </h1>
+      <P class="py-1 text-blue-600">{{ `${desc}` }}</P>
+      <button class="bg-blue-500 text-white font-normal py-2 py-4 rounded">
+        Push Me!
+      </button>
+    </div>
+    <Navigation />
   </div>
 </template>
 
 <script>
+import Navigation from '../components/Navigation.vue'
+
 export default {
   name: 'IndexPage',
+  components: { Navigation },
   data() {
     return {
       title: 'Hello, world!',
+      desc: "Index.vueに記載のあった、name:'IndexPage'をこちらに転載",
     }
   },
 }
 </script>
+<style lang="scss">
+div {
+  h1 {
+    color: #2b2b2b;
+  }
+}
+</style>
